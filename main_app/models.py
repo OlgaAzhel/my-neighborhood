947b8e32d0dd168c055a0f7cb7ea7bf1df004fd9
mbsii3 <mshortjr3@gmail.com>
2023-03-17 16:20:52 -0500

    add report model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+AGENCIES = (
+    ('P', 'Police'),
+    ('T', 'Transportation'),
+    ('F', 'Fire'),
+    ('H', 'Health'),
+    ('S', 'Sanitation'),
+    ('U', 'Utilities'),
+    ('O', 'Other'),
+)
+
+class Report(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField('Date Filed')
+    description = models.TextField(max_length=250)
+    location = models.CharField(max_length=50)
+    agency = models.CharField(
+        max_length=1,
+        choices= AGENCIES,
+        default= [0][0]
+    )
+
+    def __str__(self):
+        return f'{self.title} ({self.id})'
