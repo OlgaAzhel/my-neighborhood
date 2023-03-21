2010d41572ecd2f476e59b84b174c092be8f503f
pdsilva90 <pdsilva90@gmail.com>
2023-03-21 09:40:39 -0500

    add updateview

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Report
 
 # Create your views here.
@@ -11,7 +13,9 @@ def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
 
+
 def reports_index(request):
+  # reports = Report.objects.filter(user=request.user)
   reports = Report.objects.all()
   return render(request, 'reports/index.html', {
     'reports': reports
@@ -20,34 +24,35 @@ def reports_index(request):
 
 def reports_detail(request, report_id):
   report = Report.objects.get(id=report_id)
-  
   return render(request, 'reports/detail.html', {
     'report': report, 
   })
+  
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-   
     form = UserCreationForm(request.POST)
     if form.is_valid():
-     
       user = form.save()
-      
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
- 
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
 
-class ReportCreate(CreateView):
+class ReportCreate(LoginRequiredMixin, CreateView):
   model = Report
   fields = ['title', 'date', 'description', 'location', 'coordX', 'coordY', 'agency']
 
   def form_valid(self, form):
     form.instance.user = self.request.user 
     return super().form_valid(form)
+
+
+class ReportUpdate(LoginRequiredMixin, UpdateView):
+  model = Report
+  fields = ['title', 'date', 'description', 'location', 'coordX', 'coordY', 'agency']
