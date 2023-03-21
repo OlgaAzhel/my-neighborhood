9886a9a5fd7c90c5a5b9269304ca7d2b7d49814d
Olga Azhel <olga.azhel@gmail.com>
2023-03-21 11:26:07 -0400

    conflict resolve

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('reports/create/', views.ReportCreate.as_view(), name='report_create'),
     path('reports/<int:report_id>/', views.reports_detail, name='detail'),
-    path('reportsApi/', views.reportsApi, name='reportsApi')
+    path('reportsApi/', views.reportsApi, name='reportsApi'),
     path('reports/<int:pk>/update/', views.ReportUpdate.as_view(), name='report_update'),
 ]   
 
